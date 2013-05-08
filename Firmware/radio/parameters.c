50f6a8e7c902e1385c513d00784dc32b3019102b
Luke Hovington <lukeh@hovo.id.au>
2013-05-08 12:21:49 +1000

    Moved NodeCount to the bottom of the Pram list as this could disappear in the coming revisions

--- Firmware/radio/parameters.c
+++ Firmware/radio/parameters.c
@@ -67,9 +67,9 @@ __code const struct parameter_info {
 /*13*/  {"MANCHESTER",  0},
 /*14*/  {"RTSCTS",  0},
 /*15*/  {"NODEID",  2}, // The base node is '1' lets make new nodes 2
-/*16*/  {"NODECOUNT",  3}, // The amount of nodes in the network, this may could become auto discovery later.
-/*17*/  {"NODEDESTINATION", 65535},
-/*16*/  {"SYNCANY",  0}, // The amount of nodes in the network, this may could become auto discovery later.
+/*16*/  {"NODEDESTINATION", 65535},
+/*17*/  {"SYNCANY",  0}, // The amount of nodes in the network, this may could become auto discovery later.
+/*18*/  {"NODECOUNT",  3}, // The amount of nodes in the network, this may could become auto discovery later.
 };
 
 /// In-RAM parameter store.
--- Firmware/radio/parameters.h
+++ Firmware/radio/parameters.h
@@ -62,9 +62,9 @@ enum ParamID {
         PARAM_MANCHESTER,     // enable manchester encoding
         PARAM_RTSCTS,         // enable hardware flow control
         PARAM_NODEID,         // node ID
-        PARAM_NODECOUNT,      // number of sequential nodes in the network
         PARAM_NODEDESTINATION,// packet destination
         PARAM_SYNCANY,        // Let this node sync from any in the network not just the base
+        PARAM_NODECOUNT,      // number of sequential nodes in the network
         PARAM_MAX             // must be last
 };
 
